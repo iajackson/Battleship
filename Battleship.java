5b3b7ec21dfba7c7dcb64a87e025601e29bbe3d5
Ian Jackson <34674321+iajackson@users.noreply.github.com>
2019-01-31 14:44:56 +1000

    Start of new simpler version

--- Battleship.java
+++ Battleship.java
@@ -1,18 +1,8 @@
 /*
-Prompt for number of players
-Prompt for size of gameboard
-Prompt for number of ships per player
-Generate gameboard
-Ship placement phase
-Combat phase
-End game phase
-*/
-
-/*
 first draft
 assume 1 player
-assume 9x9 gameboard
-assume 5 ships
+assume 3x3 gameboard
+assume 1 ship
 */
 import java.util.Scanner;
 import java.util.Random;
@@ -22,10 +12,58 @@ public class Battleship
     public Battleship()
     {
         //Scanner keyboard = new Scanner(System.in);
-        Random rn = new Random();
+        Random randomGenerator = new Random();
+        final int GAMEBOARD_SIZE = 15;
         
-    }
+        // Generate gameboard
+        //String gameBoard[][] = {{"1", "2", "3"},
+        //                        {"4", "5", "6"},
+        //                        {"7", "8", "9"}};
+        String gameBoard[][] = new String[GAMEBOARD_SIZE][GAMEBOARD_SIZE];
+        for (int row = 0; row < GAMEBOARD_SIZE; row++)
+        {
+            for (int col = 0; col < GAMEBOARD_SIZE; col++)
+            {
+                gameBoard[row][col] = " ";
+            }
+        }
 
+        // Print gameboard
+        // Print top border
+        for (int i = 0; i < (2 * gameBoard.length + 1); i++)
+        {
+            System.out.print("#");
+        }
+        System.out.print("\n");
+        for (int row = 0; row < gameBoard.length; row++)
+        {
+            System.out.print("#"); // Print left border
+            for (int col = 0; col < (2 * gameBoard[row].length - 1); col++)
+            {
+                int colCount = 0;
+                if (col % 2 == 0) // Alternate space and border
+                {
+                    System.out.print(gameBoard[row][colCount]);
+                }
+                else
+                    System.out.print("|");
+                colCount++;
+            }
+            System.out.print("#"); // Print right border
+            System.out.print("\n");
+        }
+        // Print bottom border
+        for (int i = 0; i < (2 * gameBoard.length + 1); i++)
+        {
+            System.out.print("#");
+        }
+        System.out.print("\n");
+        
+        // Place ship on board
+        // Prompt for guess
+        // Repeat until guess count reached or ship sunk 
+    }
+    
     public static void main(String[] args)
     {
         new Battleship();
